42971115cb2bb6bd764f7c194c947d72b8adc7aa
DIMON3-tech <66385367+DIMON3-tech@users.noreply.github.com>
2022-10-04 22:18:53 +0300

    added file code_2.cpp

---
+++ code_2.cpp
@@ -0,0 +1,9 @@
+//
+// Created by dimab on 04.10.2022.
+//
+#include <iostream>
+int main(){
+    std::cout<<"dlksfj";
+    return 0;
+}
+#include "code_2.h"
---
+++ code_2.h
@@ -0,0 +1,14 @@
+//
+// Created by dimab on 04.10.2022.
+//
+
+#ifndef GIT_LABORATORY_WORK_2_CODE_2_H
+#define GIT_LABORATORY_WORK_2_CODE_2_H
+
+
+class code_2 {
+
+};
+
+
+#endif //GIT_LABORATORY_WORK_2_CODE_2_H
