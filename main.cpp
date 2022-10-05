092cd53699f8edac61d8b57baa9a3fd1a007a6c4
Konstantin <tymkovskiy1337@gmail.com>
2022-10-05 16:57:58 +0300

    Fixed some bugs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout <<"slkdfjsldkfj";
+    std::cout << "Hello!" << std::endl;
+    std::cout <<"World!";
     return 0;
 }
